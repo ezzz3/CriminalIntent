465efd4d6c94013676813f52a2da1d6c709d0d95
Jorge Garcia <jgarcialope2021@student.palomar.edu>
2019-12-07 15:21:13 -0800

    Working app

---
+++ app/src/main/java/android/bignerdranch/com/CrimeFragment.java
@@ -0,0 +1,93 @@
+package android.bignerdranch.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.UUID;
+
+public class CrimeFragment extends Fragment
+{
+  private static final String ARG_CRIME_ID = "crime_id";
+  private Crime mCrime;
+  private EditText mTitleField;
+  private Button mDateButton;
+  private CheckBox mSolvedCheckBox;
+
+  public static CrimeFragment newInstance(UUID crimeId)
+  {
+    Bundle args = new Bundle();
+    args.putSerializable(ARG_CRIME_ID, crimeId);
+
+    CrimeFragment fragment = new CrimeFragment();
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    UUID crimeID = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+    mCrime = CrimeLab.get(getActivity()).getCrime(crimeID);
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+  {
+    View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+    mTitleField = v.findViewById(R.id.crime_title);
+    mTitleField.setText(mCrime.getTitle());
+    mTitleField.addTextChangedListener(new TextWatcher()
+    {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after)
+      {
+        //This space intentionally left blank
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count)
+      {
+        mCrime.setTitle(s.toString());
+      }
+
+      @Override
+      public void afterTextChanged(Editable s)
+      {
+        //This one too
+      }
+    });
+
+    mDateButton = v.findViewById(R.id.crime_date);
+    mDateButton.setText(mCrime.getDate().toString());
+    mDateButton.setEnabled(false);
+
+    mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+    mSolvedCheckBox.setChecked(mCrime.isSolved());
+    mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+    {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+      {
+        mCrime.setSolved(isChecked);
+      }
+    });
+
+    return v;
+  }
+}
