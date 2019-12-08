aa7f02507b599b677757b5febacc7e2f6a91600b
Jorge Garcia <jgarcialope2021@student.palomar.edu>
2019-12-07 16:27:29 -0800

    Date changes in DatePickerFragment get reflected in CrimeFragment

--- app/src/main/java/android/bignerdranch/com/CrimeFragment.java
+++ app/src/main/java/android/bignerdranch/com/CrimeFragment.java
@@ -1,6 +1,7 @@
 package android.bignerdranch.com;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -17,12 +18,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class CrimeFragment extends Fragment
 {
   private static final String ARG_CRIME_ID = "crime_id";
   private static final String DIALOG_DATE = "DialogDate";
+
+  public static final int REQUEST_DATE = 0;
 
   private Crime mCrime;
   private EditText mTitleField;
@@ -77,7 +81,7 @@ public class CrimeFragment extends Fragment
     });
 
     mDateButton = v.findViewById(R.id.crime_date);
-    mDateButton.setText(mCrime.getDate().toString());
+    updateDate();
     mDateButton.setOnClickListener(new View.OnClickListener()
     {
       @Override
@@ -86,6 +90,7 @@ public class CrimeFragment extends Fragment
         FragmentManager manager = getFragmentManager();
         DatePickerFragment dialog = DatePickerFragment
                 .newInstance(mCrime.getDate());
+        dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
         dialog.show(manager, DIALOG_DATE);
       }
     });
@@ -102,5 +107,27 @@ public class CrimeFragment extends Fragment
     });
 
     return v;
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+  {
+    if (resultCode != Activity.RESULT_OK)
+    {
+      return;
+    }
+
+    if (requestCode == REQUEST_DATE)
+    {
+      Date date = (Date) data
+              .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+      mCrime.setDate(date);
+      updateDate();
+    }
+  }
+
+  private void updateDate()
+  {
+    mDateButton.setText(mCrime.getDate().toString());
   }
 }
--- app/src/main/java/android/bignerdranch/com/DatePickerFragment.java
+++ app/src/main/java/android/bignerdranch/com/DatePickerFragment.java
@@ -1,6 +1,9 @@
 package android.bignerdranch.com;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +16,11 @@ import androidx.fragment.app.DialogFragment;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DatePickerFragment extends DialogFragment
 {
+  public static final String EXTRA_DATE = "com.bignerdranch.android.criminalintent.date";
   public static final String ARG_DATE = "date";
 
   private DatePicker mDatePicker;
@@ -51,9 +56,32 @@ public class DatePickerFragment extends DialogFragment
     return new AlertDialog.Builder(getActivity())
             .setView(view)
             .setTitle(R.string.date_picker_title)
-            .setPositiveButton(android.R.string.ok, null)
+            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+            {
+              @Override
+              public void onClick(DialogInterface dialog, int which)
+              {
+                int year = mDatePicker.getYear();
+                int month = mDatePicker.getMonth();
+                int day = mDatePicker.getDayOfMonth();
+                Date date = new GregorianCalendar(year, month, day).getTime();
+                sendResult(Activity.RESULT_OK, date);
+              }
+            })
             .create();
   }
 
+  private void sendResult(int resultCode, Date date)
+  {
+    if (getTargetFragment() == null)
+    {
+      return;
+    }
+
+    Intent intent = new Intent();
+    intent.putExtra(EXTRA_DATE, date);
+
+    getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
+  }
 
 }
